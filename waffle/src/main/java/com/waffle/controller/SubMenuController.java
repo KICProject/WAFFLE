e7f4fcb8af9086933026394d1d46799a88f86c89
jonghyuk park <jkzombie7@gmail.com>
2020-04-20 15:59:03 +0900

    target commit

---
+++ waffle/src/main/java/com/waffle/controller/SubMenuController.java
@@ -0,0 +1,24 @@
+package com.waffle.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/sub/*")
+public class SubMenuController {
+
+	private static final Logger logger = LoggerFactory.getLogger(SubMenuController.class);
+	
+	@RequestMapping(value="/about", method = RequestMethod.GET)
+	public String moveAbout() throws Exception{			
+		return "/sub/about";
+	}
+	@RequestMapping(value="/waffleService", method = RequestMethod.GET)
+	public String moveWaffleService() throws Exception{			
+		return "/sub/waffleService";
+	}
+	
+}
